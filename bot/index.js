ba3784f6fd319dd8c7e8ffdf23531b6f858c1160
Stephen Geller <stephen.geller@hotmail.com>
2018-06-11 15:37:04 +0100

    set up basic bot and api directories

---
+++ bot/index.js
@@ -0,0 +1,29 @@
+var Twit = require('twit');
+var axios = require('axios');
+
+var bot = new Twit({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+	timeout_ms: 60 * 1000
+});
+
+let arr = ['foo', 'bar', 'baz'];
+var item = arr[Math.floor(Math.random() * arr.length)];
+let d = new Date();
+let url = 'http://localhost:3000/data';
+
+function postToTwitter(message) {
+	bot.post('statuses/update', { status: message }, (err, data, response) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data.text + ' was tweeted');
+		}
+	});
+}
+
+axios.get(url).then(r => {
+	postToTwitter(r.data.comingFromApi);
+});
