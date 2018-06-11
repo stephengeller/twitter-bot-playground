ce502ca94bf6f1d2775c9ab6cc450b16f67a9ddb
Stephen Geller <stephen.geller@hotmail.com>
2018-06-11 16:08:45 +0100

    cleaned up routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 router.get('/', function(req, res, next) {
-	res.send({ comingFromApi: true });
+	let url = 'https://jsonplaceholder.typicode.com/posts';
+	axios.get(url).then(r => {
+		const message = r.data[0].title;
+		console.log(message);
+		res.send({ comingFromApi: message });
+	});
 });
 
 module.exports = router;
