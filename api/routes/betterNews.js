bc4820b8db69d52b8e9158125a47ed4dcf7c0307
Stephen Geller <stephen.geller@hotmail.com>
2018-06-12 12:07:45 +0100

    added dotenv, configure basic news api queries

--- api/routes/betterNews.js
+++ api/routes/betterNews.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var dotenv = require('dotenv').config();
+
+// axios.defaults.headers.common['Authorization'] = process.env.NEWS_API_KEY;
 
 router.get('/', function(req, res) {
-	res.send({ message: 'better news' });
+	let headers = `?country=gb&apiKey=${process.env.NEWS_API_KEY}`;
+	let url = 'https://newsapi.org/v2/top-headlines' + headers;
+	axios
+		.get(url, headers)
+		.then(r => {
+			const { articles } = r.data;
+			articles.forEach(article => console.log(article.title));
+			res.send({ message: 'news' });
+		})
+		.catch(err => console.log(err.response.data));
 });
 
 const replaceWordInString = (string, oldWord, newWord) => {
