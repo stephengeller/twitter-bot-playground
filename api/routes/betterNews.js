e1119ca91bee473dbb7c8617cc6d24a98e831f20
Stephen Geller <stephen.geller@hotmail.com>
2018-06-12 14:10:30 +0100

    loop over json words to replace in strings

--- api/routes/betterNews.js
+++ api/routes/betterNews.js
@@ -4,11 +4,14 @@ const axios = require('axios');
 const NewsAPI = require('newsapi');
 const dotenv = require('dotenv').config();
 const StringReplacer = require('../src/stringReplacer');
+const jsonData = require('../../data/words.json');
+const apiKey = process.env.NEWS_API_KEY;
+const stringReplacer = new StringReplacer();
+const newsapi = new NewsAPI(apiKey);
 
 router.get('/', function(req, res) {
-	const apiKey = process.env.NEWS_API_KEY;
-	const stringReplacer = new StringReplacer();
-	const newsapi = new NewsAPI(apiKey);
+	let title = '';
+	let responseData = [];
 
 	newsapi.v2
 		.topHeadlines({
@@ -17,21 +20,17 @@ router.get('/', function(req, res) {
 		.then(response => {
 			const { articles } = response;
 			articles.forEach(article => {
-				let title = article.title
-					.replaceAll('resigns', 'decides to bounce')
-					.replaceAll('Brexiteers', 'fucking idiots')
-					.replaceAll('Brexit', 'FuckOuttaEuro')
-					.replaceAll('actor', 'pretendyperson')
-					.replaceAll('Trump', 'StupidMcPumpkinFace')
-					.replaceAll('memes', 'dank maymays');
-				console.log(title);
-				console.log(article.url, '\n');
+				let { title } = article;
+				for (let i in jsonData) {
+					let obj = jsonData[i];
+					title = title.replaceAll(obj.wordToReplace, obj.wordToAdd);
+				}
+				responseData.push(title);
 			});
-			res.send({ response: true });
+			res.send({ response: responseData });
 		})
 		.catch(err => {
-			console.log(err);
-			res.send({ response: false });
+			res.send({ response: false, err: err });
 		});
 });
 
