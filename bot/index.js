3cc0366a6ec9440bfa6691b1dfa91085a0854479
Stephen Geller <stephen.geller@hotmail.com>
2018-06-14 09:16:10 +0000

    added dotenv dependency to bot

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,10 @@
 const Twit = require('twit');
 const axios = require('axios');
+const dotenv = require('dotenv').config()
+
+const {TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET} = process.env
+
+// console.log(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 
 const bot = new Twit({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
